724e018f5653b6f1501bd5c0d668bd9dc92050ec
Rajarshi Mondal <88313261+Rajarshi0@users.noreply.github.com>
2023-09-21 08:45:29 +0530

    Add files via upload

---
+++ ce11_ref_genome_conf.py
@@ -0,0 +1,11 @@
+import_reference_genomes = {
+    "ce11": {
+        "name": "WBcel235",
+        "species": "Caenorhabditis elegans",
+        "chromosomes": OrderedDict([
+            ("chrI", (15072434,"A")), ("chrII", (15279421,"A")), ("chrIII", (13783801,"A")), ("chrIV", (17493829,"A")), 
+            ("chrV", (20924180,"A")), ("chrX", (17718942,"A")), ("chrM", (13794,"A"))
+        ]),
+        "gc_file":"ce11_gc_file.pytor",
+            }
+}
